e4157bee1b21eb3becb6d088535ffa241870b780
phrlog <kh@linkprofit.com>
2018-02-05 19:54:54 +0300

    добавил класс LeadService

---
+++ src/services/LeadService.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace linkprofit\AmoCRM\services;
+
+use linkprofit\AmoCRM\entities\EntityInterface;
+use linkprofit\AmoCRM\RequestHandler;
+
+class LeadService implements ServiceInterface
+{
+    /**
+     * @var RequestHandler
+     */
+    protected $request;
+
+    /**
+     * @var array
+     */
+    protected $fields = [];
+
+    /**
+     * @var
+     */
+    protected $response;
+
+    /**
+     * @var array Lead
+     */
+    protected $lead = [];
+
+    /**
+     * LeadService constructor.
+     * @param RequestHandler $requestHandler
+     */
+    public function __construct(RequestHandler $requestHandler)
+    {
+        $this->request = $requestHandler;
+    }
+
+    /**
+     * @param EntityInterface $lead
+     */
+    public function add(EntityInterface $lead)
+    {
+        $this->lead[] = $lead;
+    }
+
+    public function createLead()
+    {
+        $this->composeAddFields();
+        $this->request->performRequest($this->getLink(), $this->fields);
+        $this->response = $this->request->getResponse();
+
+        if ($this->checkResponse()) {
+            return $this->getResponse();
+        }
+
+        return false;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function checkResponse()
+    {
+        if (isset($this->response['id'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLink()
+    {
+        return 'https://' . $this->request->getSubdomain() . '.amocrm.ru/api/v2/leads';
+    }
+
+    /**
+     * Fill fields for response
+     */
+    protected function composeAddFields()
+    {
+       $fields = [];
+
+       foreach ($this->lead as $lead) {
+           $fields[] = $lead->get();
+       }
+
+        $this->fields['add'] = $fields;
+    }
+}
