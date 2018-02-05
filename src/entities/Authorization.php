3645562cb6d42ac074a4c19a3775e467818d16e9
phrlog <kh@linkprofit.com>
2018-02-05 19:51:09 +0300

    обновил класс Authorization в соответствии с EntityInterface

--- src/entities/Authorization.php
+++ src/entities/Authorization.php
@@ -2,14 +2,34 @@
 
 namespace linkprofit\AmoCRM\entities;
 
+/**
+ * Class Authorization
+ * @package linkprofit\AmoCRM\entities
+ */
 class Authorization implements EntityInterface
 {
     public $login;
     public $apiHash;
 
+    /**
+     * Authorization constructor.
+     * @param $login
+     * @param $apiHash
+     */
     public function __construct($login, $apiHash)
     {
         $this->login = $login;
         $this->apiHash = $apiHash;
+    }
+
+    /**
+     * @return array
+     */
+    public function get()
+    {
+       return [
+           'USER_LOGIN' => $this->login,
+           'USER_HASH' => $this->apiHash
+       ];
     }
 }
