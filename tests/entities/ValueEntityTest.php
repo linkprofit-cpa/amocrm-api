cf731b3be8cb63759129dbf699b5560b844cee01
phrlog <kh@linkprofit.com>
2018-02-06 18:14:46 +0300

    добавил ValueEntityTest

---
+++ tests/entities/ValueEntityTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ValueEntityTest extends TestCase
+{
+    public function testGet()
+    {
+        $value = new \linkprofit\AmoCRM\entities\Value('email@email.com', '304683');
+        $this->assertEquals(['value' => 'email@email.com', 'enum' => '304683'], $value->get());
+    }
+
+    public function testFullGet()
+    {
+        $value = new \linkprofit\AmoCRM\entities\Value('email@email.com', '304683');
+        $value->is_system = 0;
+        $value->subtype = 'subtype';
+        $this->assertEquals(['value' => 'email@email.com', 'enum' => '304683', 'is_system' => 0, 'subtype' => 'subtype'], $value->get());
+    }
+}
