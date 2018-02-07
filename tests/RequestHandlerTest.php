12b59d011c2a202193988ae2d13581ccf1fed47d
phrlog <kh@linkprofit.com>
2018-02-07 18:16:07 +0300

    добавил RequestHandlerTest

---
+++ tests/RequestHandlerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class RequestHandlerTest extends TestCase
+{
+    public function testSetSubdomain()
+    {
+        $request = new \linkprofit\AmoCRM\RequestHandler();
+        $request->setSubdomain('domain');
+
+        $this->assertEquals('domain', $request->getSubdomain());
+    }
+
+    public function testEncodeResponseSuccess()
+    {
+        $request = new \linkprofit\AmoCRM\RequestHandler();
+
+        $reflection = new \ReflectionClass(get_class($request));
+
+        $request->setSubdomain('domain');
+
+        $request->performRequest('linkdoesnotmatter', []);
+
+        $responseField = $reflection->getProperty('response');
+        $responseField->setAccessible(true);
+        $responseField->setValue($request, json_encode(['response' => ['auth' => 'success']]));
+
+        $httpCodeField = $reflection->getProperty('httpCode');
+        $httpCodeField->setAccessible(true);
+        $httpCodeField->setValue($request,  200);
+
+        $this->assertEquals(['response' => ['auth' => 'success']], $request->getResponse());
+    }
+
+    public function testEncodeResponseKnownException()
+    {
+        $request = new \linkprofit\AmoCRM\RequestHandler();
+
+        $reflection = new \ReflectionClass(get_class($request));
+
+        $request->setSubdomain('domain');
+
+        $request->performRequest('linkdoesnotmatter', []);
+
+        $responseField = $reflection->getProperty('response');
+        $responseField->setAccessible(true);
+        $responseField->setValue($request, json_encode([]));
+
+        $httpCodeField = $reflection->getProperty('httpCode');
+        $httpCodeField->setAccessible(true);
+        $httpCodeField->setValue($request,  500);
+
+        $request->getResponse();
+        $this->expectOutputString('Error: Internal server error' . PHP_EOL . 'Error code: ' . 500);
+    }
+
+    public function testEncodeResponseUnknownException()
+    {
+        $request = new \linkprofit\AmoCRM\RequestHandler();
+
+        $reflection = new \ReflectionClass(get_class($request));
+
+        $request->setSubdomain('domain');
+
+        $request->performRequest('linkdoesnotmatter', []);
+
+        $responseField = $reflection->getProperty('response');
+        $responseField->setAccessible(true);
+        $responseField->setValue($request, json_encode([]));
+
+        $httpCodeField = $reflection->getProperty('httpCode');
+        $httpCodeField->setAccessible(true);
+        $httpCodeField->setValue($request,  508);
+
+        $request->getResponse();
+        $this->expectOutputString('Error: Undescribed error' . PHP_EOL . 'Error code: ' . 508);
+    }
+}
