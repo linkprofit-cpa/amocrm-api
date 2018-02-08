b80a769cddd16aed242396349579c8f53eff6a92
phrlog <kh@linkprofit.com>
2018-02-08 14:07:15 +0300

    добавил абстрактный класс BaseService

---
+++ src/services/BaseService.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace linkprofit\AmoCRM\services;
+
+use linkprofit\AmoCRM\entities\EntityInterface;
+use linkprofit\AmoCRM\RequestHandler;
+
+abstract class BaseService implements ServiceInterface
+{
+    /**
+     * @var RequestHandler
+     */
+    protected $request;
+
+    /**
+     * @var array
+     */
+    protected $fields = [];
+
+    /**
+     * @var
+     */
+    protected $response;
+
+    /**
+     * @var array EntityInterface
+     */
+    protected $entities = [];
+
+    /**
+     * LeadService constructor.
+     * @param RequestHandler $requestHandler
+     */
+    public function __construct(RequestHandler $requestHandler)
+    {
+        $this->request = $requestHandler;
+    }
+
+    /**
+     * @return bool|mixed
+     */
+    public function create()
+    {
+        $this->composeAddFields();
+        $this->request->performRequest($this->getLink(), $this->fields);
+        $this->response = $this->request->getResponse();
+
+        if ($this->checkResponse()) {
+            return $this->getResponse();
+        }
+
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEntities()
+    {
+        return $this->entities;
+    }
+
+    /**
+     * @return array|bool
+     */
+    public function parseResponseToEntities()
+    {
+        if (!$this->checkResponse()) {
+            return false;
+        }
+        $this->entities = [];
+
+        foreach ($this->response['_embedded']['items'] as $item) {
+            $this->entities[] = $this->parseArrayToEntity($item);
+        }
+
+        return $this->entities;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function checkResponse()
+    {
+        if (isset($this->response['_embedded']['items']) && count($this->response['_embedded']['items'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Fill fields for request
+     */
+    protected function composeAddFields()
+    {
+        $fields = [];
+
+        foreach ($this->entities as $entity) {
+            $fields[] = $entity->get();
+        }
+
+        $this->fields['add'] = $fields;
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getLink();
+
+    /**
+     * @param $array
+     * @return EntityInterface
+     */
+    abstract public function parseArrayToEntity($array);
+}
