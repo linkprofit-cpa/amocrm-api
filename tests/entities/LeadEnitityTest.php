3772c44dbd055d5e1e99b8eea46347d104c5c0f7
phrlog <kh@linkprofit.com>
2018-02-09 13:15:38 +0300

    поправил ошибку в LeadEntityTest

--- tests/entities/LeadEnitityTest.php
+++ tests/entities/LeadEnitityTest.php
@@ -8,17 +8,17 @@ class LeadEntityTest extends TestCase
     {
         $lead = new \linkprofit\AmoCRM\entities\Lead();
         $lead->status_id = 17077744;
-        $lead->price = 0;
+        $lead->sale = 0;
         $lead->responsible_user_id = 1924000;
 
-        $this->assertEquals(['status_id' => '17077744', 'price' => 0, 'responsible_user_id' => 1924000], $lead->get());
+        $this->assertEquals(['status_id' => '17077744', 'sale' => 0, 'responsible_user_id' => 1924000], $lead->get());
     }
 
     public function testCustomFieldAdd()
     {
         $lead = new \linkprofit\AmoCRM\entities\Lead();
         $lead->status_id = 17077744;
-        $lead->price = 0;
+        $lead->sale = 0;
         $lead->responsible_user_id = 1924000;
 
         $customField = new \linkprofit\AmoCRM\entities\CustomField('146785', 'email', 'EMAIL');
@@ -29,7 +29,7 @@ class LeadEntityTest extends TestCase
 
         $lead->addCustomField($customField);
 
-        $this->assertEquals(['status_id' => '17077744', 'price' => 0, 'responsible_user_id' => 1924000, 'custom_fields' => [
+        $this->assertEquals(['status_id' => '17077744', 'sale' => 0, 'responsible_user_id' => 1924000, 'custom_fields' => [
            $customField->get()
         ]], $lead->get());
     }
