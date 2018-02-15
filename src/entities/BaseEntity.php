127d7a9e61762fb5601056641d345500d7c36f0b
phrlog <kh@linkprofit.com>
2018-02-15 17:27:16 +0300

    добавил класс BaseEntity

---
+++ src/entities/BaseEntity.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace linkprofit\AmoCRM\entities;
+
+use linkprofit\AmoCRM\traits\FieldList;
+
+/**
+ * Class BaseEntity
+ * @package entities
+ */
+abstract class BaseEntity implements EntityInterface
+{
+    /**
+     * @var integer id сущности
+     */
+    public $id;
+
+    /**
+     * @var string Дата и время создания сущности
+     */
+    public $created_at;
+
+    /**
+     * @var string Дата и время изменения сущности
+     */
+    public $updated_at;
+
+    /**
+     * @var integer id пользователя ответственного за сущность
+     */
+    public $responsible_user_id;
+
+    /**
+     * @var array
+     */
+    protected $fieldList = [];
+
+    use FieldList;
+
+    /**
+     * @return array
+     */
+    public function get()
+    {
+        $fields = $this->getExistedValues($this->fieldList);
+
+        return $fields;
+    }
+
+    /**
+     * @param $array
+     */
+    public function set($array)
+    {
+        $this->setFromArray($this->fieldList, $array);
+    }
+}
