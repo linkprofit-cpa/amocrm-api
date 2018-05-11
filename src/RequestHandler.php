ec5f9319982366b9fed0885a89482f0c57dc490b
Anton Kartsev <anton@alarm.ru>
2018-05-11 15:46:37 +0300

    Добавлена возможность использовать непубличное api осуществляя POST запросы к скриптам, к которым обращаются клиентские скрипты из браузера при работе в AmoCrm

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -48,9 +48,10 @@ class RequestHandler
 
     /**
      * @param string $link
-     * @param array $fields
+     * @param array  $fields
+     * @param string $contentType
      */
-    public function performRequest($link, array $fields)
+    public function performRequest($link, array $fields, $contentType = 'application/json')
     {
         $curl = curl_init();
 
@@ -58,8 +59,8 @@ class RequestHandler
         curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
         curl_setopt($curl, CURLOPT_URL, $link);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->getPostFields($fields, $contentType));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: '.$contentType]);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_COOKIEFILE, $this->getCookiePath());
         curl_setopt($curl, CURLOPT_COOKIEJAR, $this->getCookiePath());
@@ -124,5 +125,20 @@ class RequestHandler
     protected function getCookiePath()
     {
         return dirname(dirname(__FILE__)) . '/cookie.txt';
+    }
+
+    /**
+     * @param array $fields
+     * @param       $contentType
+     *
+     * @return string
+     */
+    private function getPostFields(array $fields, $contentType)
+    {
+        if ($contentType === 'application/json') {
+            return json_encode($fields);
+        }
+
+        return http_build_query($fields);
     }
 }
